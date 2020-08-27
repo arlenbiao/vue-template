90ef6a19689b9cb7522a0ef873dcd95fd4a9a41e
潘彪 <80188623@qq.com>
2020-08-27 14:47:19 +0800

    1、新增消除iPhone上点击div出现背景样式 2、body动画

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -11,25 +11,41 @@ export default {
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  * {
-    padding: 0;
-    margin: 0;
-  }
-  /* // 禁止长按图片保存 */
-  img {
-    -webkit-touch-callout: none;
-    pointer-events: none;
-    /* // 像微信浏览器还是无法禁止，加上这行样式即可 */
-  }
-  div {
-    /* // 禁止长按选择文字 */
-    -webkit-user-select: none;
-    /* // 禁止长按呼出菜单 */
-    -webkit-touch-callout: none;
-  }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+* {
+  padding: 0;
+  margin: 0;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  -webkit-tap-highlight-color: transparent;
+}
+/* // 禁止长按图片保存 */
+img {
+  -webkit-touch-callout: none;
+  pointer-events: none;
+  /* // 像微信浏览器还是无法禁止，加上这行样式即可 */
+}
+div {
+  -webkit-overflow-scrolling: touch;
+  /* // 禁止长按选择文字 */
+  -webkit-user-select: none;
+  /* // 禁止长按呼出菜单 */
+  -webkit-touch-callout: none;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  -webkit-tap-highlight-color: transparent;
+}
+li {
+  list-style: none;
+}
+body,
+html {
+  -webkit-overflow-scrolling: touch;
+}
+body {
+  -webkit-transition-duration: 400ms;
+  transition-duration: 400ms;
+}
 </style>
